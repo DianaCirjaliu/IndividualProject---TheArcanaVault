cd50c92939906c645e28b025050e8b02dcbd8505
Diana Cirjaliu <Diana.Cirjaliu@cognizant.com>
2026-02-23 14:30:41 +0200

    feat(ui):implemented the structure for the login page

--- TheArcanaVault/src/Routes.jsx
+++ TheArcanaVault/src/Routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index/Index";
+import Login from "./pages/Login/Login";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
